ebd50b8789adc939464b36e67fa8df4fc16c4657
Fer <ruizcorofer.06qgmail.com>
2023-01-31 14:46:36 -0500

    bucle que permita concatenar textos e imprima el resultado final

---
+++ src/com/company/Bucles.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Bucles {
+    public static void main(String[] args) {
+
+    String[] names = {"Pedro", "Juan", "Carlos", "Bruno" };
+
+    for(String name : names){
+        System.out.println("Los nombres son " + name);
+    }
+
+
+    }
+
+}
